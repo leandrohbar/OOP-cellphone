21c7db35ca3256771ef65b201c94f08e1e782208
Leandro Barbosa <leandroh_99@outlook.com>
2023-10-11 16:09:13 -0300

    feat: implementando a classe Nokia, Iphone e Smartphone, como solicitado no desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia(numero:"123456789", modelo:"Nokia 3310", imei:"123456789", memoria:16);
+Iphone iphone = new Iphone(numero:"987654321", modelo:"Iphone 12", imei:"987654321", memoria:128);
+
+Console.WriteLine("Iphone:");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+Console.WriteLine("Nokia:");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
